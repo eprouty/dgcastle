371d6646407e6486895b6ff81c03f23442e6c761
Eric Prouty <Eric.Prouty@viasat.com>
2017-06-06 18:06:54 -0700

    Closes #6: Add validation for the matchplay result
    * Will make sure that every result entered is of a reasonable format.

--- dgcastle/data/match.py
+++ dgcastle/data/match.py
@@ -1,16 +1,70 @@
+class ValidationException(Exception):
+    """Used to report failures to validate an input format"""
+
+def _validate_result(result):
+        """Matchplay results can have 1 of 3 different types of values...
+            1. A victory that ended before all 18 holes were played
+                "<X>&<Y> where X > 1 and X > Y >= 1 and X - Y <= 2
+            2. A victory that ended at 18 holes
+                "<X>up" where 0 < X < 3
+            3. A tie
+                a. "tie"
+        """
+        if '.' in result:
+            raise ValidationException("Result '{}' should not contain decimals".format(result))
+        elif result == "tie":
+            # Simplest case, already valid, good to go
+            pass
+        elif "up" in result:
+            tmp = result.split("up")
+            if len(tmp) != 2 or tmp[1] != '':
+                # We've got extra info here
+                raise ValidationException("Result '{}' not in the format 'Xup'".format(result))
+            try:
+                tmp = int(tmp[0])
+                if 0 < tmp < 3:
+                    # we're good here
+                    pass
+                else:
+                    raise ValueError()
+            except ValueError:
+                raise ValidationException("Result '{}' must start with an integer of value 1 or 2".format(result))
+        elif "&" in result:
+            tmp = result.split("&")
+            if len(tmp) != 2:
+                # We've got extra info here
+                raise ValidationException("Result '{}' not in the format 'X&Y'".format(result))
+            try:
+                X = int(tmp[0])
+                Y = int(tmp[1])
+                if X > 1  and X > Y >= 1 and X - Y <= 2:
+                    # we're good here
+                    pass
+                else:
+                    raise ValidationException("Result '{}' is not a valid score. Given the format 'X&Y' must have X > 1 and X > Y >= 1".format(result))
+            except ValueError:
+                raise ValidationException("Result '{}' must use integers".format(result))
+        else:
+            raise ValidationException("Result '{}' does not appear to match any valid result format".format(result))
+        return result
+
+
 class Match():
     def __init__(self, winner='', loser='', result='', dbRecord=None):
         if dbRecord:
-            self.winner = dbRecord['winner']
-            self.loser = dbRecord['loser']
-            self.result = dbRecord['result']
+            self.winner = dbRecord['winner'].strip()
+            self.loser = dbRecord['loser'].strip()
+            self.result = _validate_result(dbRecord['result'].lower().strip())
         else:
-            self.winner = winner
-            self.loser = loser
-            self.result = result
+            self.winner = winner.strip()
+            self.loser = loser.strip()
+            self.result = _validate_result(result.lower().strip())
 
     def __eq__(self, other):
         return self.winner == other.winner and self.loser == other.loser and self.result == other.result
+
+    def __repr__(self):
+        return "Winner: {}, Loser: {}, Result: {}".format(self.winner, self.loser, self.result)
 
     def insert(self, db):
         db.match_play.insert_one({'winner': self.winner,
---
+++ tests/test_match.py
@@ -0,0 +1,37 @@
+import unittest
+
+from dgcastle.data import match
+from dgcastle.data.match import ValidationException
+
+class TestResultValidation(unittest.TestCase):
+    def test_tie(self):
+        self.assertEqual(match._validate_result('tie'), 'tie')
+
+    def test_XupFormat(self):
+        self.assertEqual(match._validate_result('1up'), '1up')
+        self.assertEqual(match._validate_result('2up'), '2up')
+        self.assertRaises(ValidationException, match._validate_result, '0up')
+        self.assertRaises(ValidationException, match._validate_result, '3up')
+        self.assertRaises(ValidationException, match._validate_result, '10up')
+        self.assertRaises(ValidationException, match._validate_result, '-1up')
+        self.assertRaises(ValidationException, match._validate_result, 'twoup')
+        self.assertRaises(ValidationException, match._validate_result, 'up')
+        self.assertRaises(ValidationException, match._validate_result, '1up2')
+        self.assertRaises(ValidationException, match._validate_result, '1')
+
+    def test_XandYFormat(self):
+        self.assertEqual(match._validate_result('2&1'), '2&1')
+        self.assertEqual(match._validate_result('3&1'), '3&1')
+        self.assertRaises(ValidationException, match._validate_result, '4&1')
+        self.assertEqual(match._validate_result('4&2'), '4&2')
+        self.assertEqual(match._validate_result('10&8'), '10&8')
+        self.assertRaises(ValidationException, match._validate_result, '10&7')
+        self.assertRaises(ValidationException, match._validate_result, '4&')
+        self.assertRaises(ValidationException, match._validate_result, '&1')
+        self.assertRaises(ValidationException, match._validate_result, '&')
+        self.assertRaises(ValidationException, match._validate_result, '4')
+        self.assertRaises(ValidationException, match._validate_result, '1')
+        self.assertRaises(ValidationException, match._validate_result, '1&-1')
+        self.assertRaises(ValidationException, match._validate_result, '4.1&1')
+        self.assertRaises(ValidationException, match._validate_result, '4&1.1')
+        
